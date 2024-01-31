9f6f210098d1adfd8eb1c4311f6f83c27a68a323
alicjaz <alza408@gmail.com>
2024-01-31 21:29:14 +0100

    - added login and logout

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
 from .models import Product
 
@@ -10,3 +12,25 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']  # Keep this as lowercase 'password'
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have been logged in!")
+            return redirect('home')
+        else:
+            messages.error(request, "There was an error, please try again.")
+            return redirect('login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You have been logged out... Thanks for stopping by")
+    return redirect('home')
