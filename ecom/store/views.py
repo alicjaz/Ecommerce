c8d0c7ccc9e19bd7403952b4acab3a923f8d184d
alicjaz <alza408@gmail.com>
2024-02-02 21:54:04 +0100

    - added registration

--- ecom/store/views.py
+++ ecom/store/views.py
@@ -2,6 +2,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+from .forms import SignUpForm
 from .models import Product
 
 
@@ -34,3 +35,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out... Thanks for stopping by")
     return redirect('home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You have registered successfully! Welcome")
+        else:
+            messages.success(request, "Whoops! There was a problem registering. Please, try again")
+            return redirect('register')
+    else:
+        return render(request, 'register.html', {'form': form})
