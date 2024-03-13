d7f0016ad08d0b8e7e62dd5dfb95ac3a97789ba6
alicjaz <alza408@gmail.com>
2024-03-13 13:35:25 +0100

    - Added Extended User Profiles

--- ecom/store/admin.py
+++ ecom/store/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
 
 admin.site.register(Category)
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+
+admin.site.register(User, UserAdmin)
